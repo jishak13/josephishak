36195a1ca230200a3f2818eb7bd536cf7b43d014
jishak13 <jishak1@asu.edu>
2018-03-02 13:10:19 -0700

    added server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var http = require('http');
 var app = express();
+
+var httpServer = http.createServer(app);
+
+
+httpServer.listen(80, () => {
+    console.log("App listening on port 80");
+});
 
 app.get('/', (req,res) => {
     res.send('ello world');
-});
-
-app.listen(80, () => {
-    console.log("App listening on port 80");
 });
